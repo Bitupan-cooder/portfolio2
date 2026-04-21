644b8b222e9520e11bf1d6bc08d872149a76b893
Bitupan-cooder <149002534+Bitupan-cooder@users.noreply.github.com>
2026-04-21 17:51:01 +0530

    update

---
+++ PORTFOLIOv2-master/app/constants/footer.ts
@@ -0,0 +1,34 @@
+import { FooterLink } from "../types";
+
+export const FOOTER_LINKS: FooterLink[] = [
+  {
+    name: 'LinkedIn',
+    hoverText: 'Connect with me',
+    icon: 'icons/linkedin.svg',
+    url: 'https://www.linkedin.com/in/krishnarsoftwareengineer00000/',
+  },
+  {
+    name: 'GitHub',
+    hoverText: 'Open Sourcing',
+    icon: 'icons/github.svg',
+    url: 'https://github.com/KRISHNA0R',
+  },
+  {
+    name: 'Instagram',
+    hoverText: '@dev.krishnar',
+    icon: 'icons/instagram.svg',
+    url: 'https://www.instagram.com/dev.krishnar/',
+  },
+  {
+    name: 'Email',
+    hoverText: 'krishnar230706@gmail.com',
+    icon: 'icons/night-mode.svg',
+    url: 'mailto:krishnar230706@gmail.com',
+  },
+  {
+    name: 'Resume',
+    hoverText: 'Download Profile',
+    icon: 'icons/file.svg',
+    url: './Krishna_R_Resume.pdf',
+  }
+];
---
+++ PORTFOLIOv2-master/app/constants/projects.ts
@@ -0,0 +1,35 @@
+import { Project } from "../types";
+
+// TODO: Move this to API
+export const PROJECTS: Project[] = [
+  {
+    title: 'AuraSync',
+    date: 'Jan 2026',
+    subtext: 'A brutalist design website created for the Srijan hackathon at Jadavpur University, showcasing bold design and rapid prototyping.',
+    url: 'https://srijan26-snap-syntax-allnighters.vercel.app',
+  },
+  {
+    title: 'PaisaTrack',
+    date: 'Dec 2025',
+    subtext: 'A smart finance tracking platform built with React and Tailwind CSS, featuring interactive dashboards and financial insights.',
+    url: 'https://paisatrack-nine.vercel.app',
+  },
+  {
+    title: 'SafePay AI',
+    date: 'Nov 2025',
+    subtext: 'An AI-powered fraud detection system prototype designed for secure digital transactions and real-time security alerts.',
+    url: 'https://rival-sunset-90238895.figma.site',
+  },
+  {
+    title: 'QuickKart',
+    date: 'Oct 2025',
+    subtext: 'A fully responsive e-commerce retail platform showcasing modern UI structure and essential digital retail features.',
+    url: 'https://quick-kart-azure.vercel.app',
+  },
+  {
+    title: 'PORTFOLIOv2',
+    date: 'Feb 2026',
+    subtext: 'The current interactive 3D portfolio project, developed with Next.js, Framer Motion, and Three.js to showcase my creative engineering journey.',
+    url: 'https://github.com/KRISHNA0R/PORTFOLIOv2',
+  },
+];
